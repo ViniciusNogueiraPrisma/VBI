30e5bdb43e14d989b6a1ce6bd6eeeb79cd28e7a9
vinicius nogueira <v.francisco@riprisma.com>
2024-02-19 20:06:54 -0300

    alt

---
+++ js/carrosel.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "fade",
+  loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
