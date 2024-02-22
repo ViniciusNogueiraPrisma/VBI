108a9dc2d1f02313363e15c438e409cdd947b51a
Vinicius Nogueira <v.francisco@riprisma.com>
2024-02-22 17:41:10 -0300

    alt

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,9 +1,18 @@
+var swiperTime = new Swiper(".swiper-time", {
+  slidesPerView: "auto",
+  spaceBetween: "24",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 var swiper = new Swiper(".mySwiper", {
   effect: "fade",
   loop: true,
-  // autoplay: {
-  //   delay: 5000,
-  // },
+  autoplay: {
+    delay: 5000,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
